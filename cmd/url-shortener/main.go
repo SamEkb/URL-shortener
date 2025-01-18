376eac532599bb766d9af815c0aaa4d631443f4d
Semyon Kilanov <sam.kilanoff@gmail.com>
2025-01-18 12:37:13 +0500

    add config, logger and database connection

---
+++ cmd/url-shortener/main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"URL-shortener/internal/config"
+	"URL-shortener/internal/storage/sqlite"
+	"log/slog"
+	"os"
+)
+
+const (
+	envLocal = "local"
+	envDev   = "dev"
+	envProd  = "prod"
+)
+
+func main() {
+	cfg := config.MustLoad()
+
+	log := setupLogger(cfg.Env)
+
+	log.Info("starting url shortener", slog.String("env", cfg.Env))
+	log.Debug("debug messages are enabled")
+
+	storage, err := sqlite.New(cfg.StoragePath)
+	if err != nil {
+		slog.Error("failed to init storage", "err", err)
+		os.Exit(1)
+	}
+
+	_ = storage
+	// TODO: init router: chi
+
+	// TODO: run server
+
+}
+
+func setupLogger(env string) *slog.Logger {
+	var logger *slog.Logger
+
+	switch env {
+	case envLocal:
+		logger = slog.New(
+			slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envDev:
+		logger = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envProd:
+		logger = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}),
+		)
+	}
+
+	return logger
+}
